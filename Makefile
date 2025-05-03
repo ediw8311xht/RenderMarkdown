5f6fb5768075030b474448ab9fb55026c6b028cf
ediw8311xht <maxballard333@gmail.com>
2025-05-03 10:45:12 -0400

    Z

--- Makefile
+++ Makefile
@@ -1,10 +1,35 @@
 .RECIPEPREFIX := $() $()
 CXX = g++
 RM = rm -f
-# CXXFLAGS=-Wall -std=c++20
-# LDLIBS=-lwthttp -lwt -lwtdbo -lwtdbosqlite3
-CPPFLAGS = -I/usr/include/opencv4 -Wall -std=c++20
-LDLIBS = -lopencv_gapi -lopencv_stitching -lopencv_alphamat -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_cvv -lopencv_dnn_objdetect -lopencv_dnn_superres -lopencv_dpm -lopencv_face -lopencv_freetype -lopencv_fuzzy -lopencv_hdf -lopencv_hfs -lopencv_img_hash -lopencv_intensity_transform -lopencv_line_descriptor -lopencv_mcc -lopencv_quality -lopencv_rapid -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_signal -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_highgui -lopencv_datasets -lopencv_text -lopencv_plot -lopencv_videostab -lopencv_videoio -lopencv_viz -lopencv_wechat_qrcode -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_video -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_imgcodecs -lopencv_features2d -lopencv_dnn -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core
+CXXFLAGS=-Wall -std=c++20
+# CPPFLAGS = -I/usr/include/opencv4 -Wall -std=c++20
+# LDLIBS = \
+# 			-lopencv_gapi             -lopencv_stitching        -lopencv_alphamat               \
+# 			-lopencv_aruco            -lopencv_bgsegm           -lopencv_bioinspired            \
+# 			-lopencv_ccalib           -lopencv_cvv              -lopencv_dnn_objdetect          \
+# 			-lopencv_dnn_superres     -lopencv_dpm              -lopencv_face                   \
+# 			-lopencv_freetype         -lopencv_fuzzy            -lopencv_hdf                    \
+# 			-lopencv_hfs              -lopencv_img_hash         -lopencv_intensity_transform    \
+# 			-lopencv_line_descriptor  -lopencv_mcc                                              \
+# 			-lopencv_quality          -lopencv_rapid            -lopencv_reg                    \
+# 			-lopencv_rgbd             -lopencv_saliency         -lopencv_signal                 \
+# 			-lopencv_stereo           -lopencv_structured_light                                 \
+# 			-lopencv_phase_unwrapping -lopencv_superres                                         \
+# 			-lopencv_optflow          -lopencv_surface_matching -lopencv_tracking               \
+# 			-lopencv_highgui          -lopencv_datasets         -lopencv_text                   \
+# 			-lopencv_plot             -lopencv_videostab        -lopencv_videoio                \
+# 			-lopencv_viz              -lopencv_wechat_qrcode    -lopencv_xfeatures2d            \
+# 			-lopencv_shape            -lopencv_ml               -lopencv_ximgproc               \
+# 			-lopencv_video            -lopencv_xobjdetect       -lopencv_objdetect              \
+# 			-lopencv_calib3d          -lopencv_imgcodecs        -lopencv_features2d             \
+# 			-lopencv_dnn              -lopencv_flann            -lopencv_xphoto                 \
+# 			-lopencv_photo            -lopencv_imgproc          -lopencv_core                   \
+CPPFLAGS = -I/usr/include/ImageMagick-7  -Wall -std=c++20
+LDLIBS = \
+		-fopenmp                      -DMAGICKCORE_HDRI_ENABLE=1          \
+		-DMAGICKCORE_QUANTUM_DEPTH=16 -DMAGICKCORE_CHANNEL_MASK_DEPTH=32  \
+		-lMagick++-7.Q16HDRI          -lMagickWand-7.Q16HDRI              \
+		-lMagickCore-7.Q16HDRI
 
 SRC_DIR = src
 SRCS = $(wildcard $(addsuffix /*.cpp, $(SRC_DIR)))
--- src/parse_markdown.h
+++ src/parse_markdown.h
@@ -18,108 +18,107 @@
 
 namespace ParseMarkdownNS {
 
-    // Declare before defining.
-    typedef struct MyToken MyToken;
-    enum MyTokenType : int;
-    class ParseMarkdown;
+// Declare before defining.
+typedef struct MyToken MyToken;
+enum MyTokenType : int;
+class ParseMarkdown;
 
-    // Instead of using namespace std which can cause conflicts or `std::`
-    // I think this is a simpler way, but maybe not "standard" or "good" practice lol
-    using _s   = std::string;
-    using _ft  = std::function<std::vector<MyToken>(std::string)>;
-    using _t   = MyToken;
+// Instead of using namespace std which can cause conflicts or `std::`
+// I think this is a simpler way, but maybe not "standard" or "good" practice lol
+using _s   = std::string;
+using _ft  = std::function<std::vector<MyToken>(std::string)>;
+using _t   = MyToken;
 
-    using _vc_s  = std::vector<std::string>;
-    using _vc_t  = std::vector<MyToken>;
-    using _vc_os = std::vector<std::optional<std::string>>;
+using _vc_s  = std::vector<std::string>;
+using _vc_t  = std::vector<MyToken>;
+using _vc_os = std::vector<std::optional<std::string>>;
 
-    using _st_t = std::set<MyToken>;
-    // Stores token the regex to find it and the lambda function to call with it.
-    using _mp_t = std::map< MyTokenType, std::tuple<const std::regex, _ft>
-                        >;
-    using _mtt  = MyTokenType;
-    using std::format;
+using _st_t = std::set<MyToken>;
+// Stores token the regex to find it and the lambda function to call with it.
+using _mp_t = std::map< MyTokenType, std::tuple<const std::regex, _ft> >;
+using _mtt  = MyTokenType;
+using std::format;
 
-    // Order to check state
-    enum MyTokenType : int {
-        none   = 0 ,
-        header     ,
-        code       ,
-        bold       ,
-        italic     ,
-        text       ,
-    };
+// Order to check state
+enum MyTokenType : int {
+    none   = 0 ,
+    header     ,
+    code       ,
+    bold       ,
+    italic     ,
+    text       ,
+};
 
-    struct MyToken {
-        _s            data;
-        bool         start;
-        MyTokenType   type;
-    };
+struct MyToken {
+    _s            data;
+    bool         start;
+    MyTokenType   type;
+};
 
-    class ParseMarkdown {
+class ParseMarkdown {
 
-        private:
-            static const _mp_t token_regex;
-            _vc_s       files; // Names of markdown files
-            _vc_os  str_files; // Lines of files
-            _s      total_str; // Concenated contents of all files
-            _vc_s      errors;
-            _vc_t   token_out; // Where tokenized markdown is store
-            inline static constexpr auto header_lambda = [](_s s) -> _vc_t { return {}; };
-            // Internal function for read_in_files()
-            // optional<string> so value returned can indicate when there is an
-            // issue with opening the file.
-            std::optional<_s> file_as_string(_s file_string) {
-                std::ifstream file(file_string);
-                if ( file.is_open() ) {
-                    std::stringstream s;
-                    return s << file.rdbuf(), s.str();
-                }
-                return std::nullopt;
+    private:
+        static const _mp_t token_regex;
+        _vc_s       files; // Names of markdown files
+        _vc_os  str_files; // Lines of files
+        _s      total_str; // Concenated contents of all files
+        _vc_s      errors;
+        _vc_t   token_out; // Where tokenized markdown is store
+        inline static constexpr auto header_lambda = [](_s s) -> _vc_t { return {}; };
+        // Internal function for read_in_files()
+        // optional<string> so value returned can indicate when there is an
+        // issue with opening the file.
+        std::optional<_s> file_as_string(_s file_string) {
+            std::ifstream file(file_string);
+            if ( file.is_open() ) {
+                std::stringstream s;
+                return s << file.rdbuf(), s.str();
             }
-            void _read_in_files(_vc_s& f, _vc_s::iterator i, _vc_s::iterator e) {
-                if (i == e) { return; }
-                std::optional<_s> o = file_as_string(*i);
-                std::cout << "HI" << std::endl;
-                if (!o.has_value()) {
-                    this->str_files.push_back("");
-                    this->errors.push_back(format("{}: file '{}' couldn't be opened.", "_read_in_files", (*i)));
-                }
-                else {
-                    this->str_files.push_back(o);
-                    this->total_str += o.value();
-                }
-                return _read_in_files(f, ++i, e);
-            }
-            void handle_regex() {
-            }
-            void _to_tokens(_st_t states, _mtt curr_toke=_mtt::none);
-        public:
-            ParseMarkdown(_vc_s files={}) : files(files) {
-                read_in_files();
-            }
-            ParseMarkdown(_s file) { ParseMarkdown({file}); }
-
-
-            void to_tokens(_st_t states) { this->token_out = {}; _to_tokens({}); }
-
-            void read_in_files() {
-                this->files = {}; this->str_files = {}; this->total_str = "";
-                _read_in_files(this->files, this->files.begin(), this->files.end());
-            }
-    };
-    inline const _mp_t ParseMarkdown::token_regex = {
-        { MyTokenType::header, { std::regex("") , header_lambda                    }  },                        
-        { MyTokenType::code,   { std::regex("") , [](_s s) -> _vc_t { return {}; } } },
-        { MyTokenType::bold,   { std::regex("") , [](_s s) -> _vc_t { return {}; } } },
-        { MyTokenType::italic, { std::regex("") , [](_s s) -> _vc_t { return {}; } } },
-        { MyTokenType::text,   { std::regex("") , [](_s s) -> _vc_t { return {}; } } },
-    };
-
-    inline void ParseMarkdown::_to_tokens(_st_t states, _mtt curr_toke) {
-        if (curr_toke == _mtt::none) {
+            return std::nullopt;
         }
-    }
+        void _read_in_files(_vc_s& f, _vc_s::iterator i, _vc_s::iterator e) {
+            if (i == e) { return; }
+            std::optional<_s> o = file_as_string(*i);
+            this->str_files.push_back(o);
+            if (!o.has_value()) {
+                this->errors.push_back(format("{}: file '{}' couldn't be opened.", "_read_in_files", (*i)));
+            }
+            else {
+                this->total_str += o.value();
+            }
+            return _read_in_files(f, ++i, e);
+        }
+        void handle_regex() {
+        }
+        inline void _to_tokens(_s curr_string) {
+            for (const auto& [key, value] : token_regex) {
+            }
+        }
+    public:
+        ParseMarkdown(_vc_s files={}) : files(files) {
+            read_in_files();
+        }
+        ParseMarkdown(_s file) { ParseMarkdown({file}); }
+
+
+        _vc_t to_tokens(_st_t states) {
+            this->token_out = {};
+            _to_tokens(this->total_str); return {};
+        }
+
+        void read_in_files() {
+            this->files = {}; this->str_files = {}; this->total_str = "";
+            _read_in_files(this->files, this->files.begin(), this->files.end());
+        }
+};
+inline const _mp_t ParseMarkdown::token_regex = {
+    { MyTokenType::header,  {    std::regex("^([#]{1,5})[ ](.*)$")      , header_lambda } },
+    { MyTokenType::code,    {    std::regex("```")                      , [](_s s) -> _vc_t { return {}; } } },
+    { MyTokenType::bold,    {    std::regex("([\\*]{2})(.*)([\\*]{2})") , [](_s s) -> _vc_t { return {}; } } },
+    { MyTokenType::italic,  {    std::regex("([*])(.*)([*])")           , [](_s s) -> _vc_t { return {}; } } },
+    { MyTokenType::text,    {    std::regex("")                         , [](_s s) -> _vc_t { return {}; } } },
+};
+
 }
 
 
