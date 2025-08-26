78ccbb2e658c4e7f01dbd6fdcc634a256acf0342
ediw8311xht <maxballard333@gmail.com>
2025-08-26 00:41:11 -0400

    fix comments in wrong place

--- src/parse_markdown.h
+++ src/parse_markdown.h
@@ -21,21 +21,20 @@ class ParseMarkdown {
         static const reg_con inline_regex;
         //----------------------------- Vars ---------------//
         std::set<_s> files = {};               // Names of markdown files
-        // std::set<std::optional<_s>> str_files; // Lines of files
         _s total_str;                          // Concenated contents of all files
         //----------------------------- Functions ----------//
+        /* Text must be cleaned since both pango and Magick++ use certain
+           characters for formatting Inline for formatting that is done within
+           the text using pango */
+        _s clean_text(_s s);
         /* Internal function for read_in_files()
            optional<string> so value returned can indicate when there is an
            issue with opening the file. */
-        _s clean_text(_s s);
         void _read_in_files( std::set<_s>& f, std::set<_s>::iterator i, std::set<_s>::iterator e );
         void handle_code(   MakeImageNS::MakeImage& mimg, const boost::match_results<_s::const_iterator>& res );
         void handle_header( MakeImageNS::MakeImage& mimg, const boost::match_results<_s::const_iterator>& res );
         void handle_image(  MakeImageNS::MakeImage& mimg, const boost::match_results<_s::const_iterator>& res );
         void handle_line(   MakeImageNS::MakeImage& mimg, const boost::match_results<_s::const_iterator>& res );
-        /* Text must be cleaned since both pango and Magick++ use certain
-           characters for formatting Inline for formatting that is done within
-           the text using pango */
         void handle_inline(MakeImageNS::MakeImage& mimg, _s s);
     public:
         // Explicit since reading in files happens when intialized.
